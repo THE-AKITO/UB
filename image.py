8eea48a2c380ec8e83fac9063eb3c29e79790832
AKITO <dipeshkoli86@gmail.com>
2024-11-15 10:26:59 +0530

    Update image.py

--- image.py
+++ image.py
@@ -1,2 +1,2 @@
-AFK_IMG = "https://graph.org/file/ca042e845339f73cde0f6.jpg"
-ALIVE_IMG = "https://graph.org/file/054a99dc43dc45e772e4a.jpg"
+AFK_IMG = "http://ibb.co/gF9yR60"
+ALIVE_IMG = "http://ibb.co/r3rv3Wg"
